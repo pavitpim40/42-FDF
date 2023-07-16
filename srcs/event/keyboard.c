628f96209c00b0f6ae792aefabd0fc35a9773e68
Pavit Pimchanagul <ppimchan@z3t3c1.42bangkok.com>
2023-07-16 20:34:13 +0700

    norm:event

--- srcs/event/keyboard.c
+++ srcs/event/keyboard.c
@@ -6,100 +6,83 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 17:17:11 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/07/16 15:10:00 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/07/16 20:32:20 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-
-void key_projection(int keycode, t_fdf *f)
+void	key_projection(int keycode, t_fdf *f)
 {
-	// isometric projection I = 34
-	if(keycode == 34)
+	if (keycode == 34)
 		isometric_projection(f);
-	// topview projection P = 35
 	if (keycode == 35)
 		topview_projection(f);
 }
-void key_offset(int keycode, t_fdf *f)
+
+void	key_offset(int keycode, t_fdf *f)
 {
 	if (keycode == 124)
 		shift_rigth(f);
-	// left = 123
-	if (keycode == 123)
+	else if (keycode == 123)
 		shift_left(f);
-	// up = 126
-	if (keycode == 126)
+	else if (keycode == 126)
 		shift_up(f);
-	// down = 125
-	if (keycode == 125)
+	else if (keycode == 125)
 		shift_down(f);
 }
 
-void key_rotate(int keycode, t_fdf *f)
+void	key_rotate(int keycode, t_fdf *f)
 {
-	// rotate-x
-	if(keycode == 18) 
+	if (keycode == 18) 
 		alpha_increase(f);
-	if(keycode == 19)
+	else if (keycode == 19)
 		alpha_decrease(f);
-	// rotate-y
-	if(keycode == 20) 
+	else if (keycode == 20) 
 		beta_increase(f);
-	if(keycode == 21)
+	else if (keycode == 21)
 		beta_decrease(f);
-	// rotate-z
-	if(keycode == 22) 
+	else if (keycode == 22) 
 		gamma_increase(f);
-	if(keycode == 23)
+	else if (keycode == 23)
 		gamma_decrease(f);
 }
 
-
-void key_altitude(int keycode, t_fdf *f)
+void	key_altitude(int keycode, t_fdf *f)
 {
-// z_divisor inc 43
-	if(keycode == 43) {
+	if (keycode == 43)
+	{
 		f->camera->z_divisor += 0.1;
 		rerender(f);
 	}
-	// z_divisor dec 47
-	if(keycode == 47) {
-		if(f->camera->z_divisor > 1)
+	else if (keycode == 47)
+	{
+		if (f->camera->z_divisor > 1)
 			f->camera->z_divisor -= 0.1;
 		rerender(f);
 	}
 }
+
 int	key_hook(int keycode, t_fdf *f)
 {
-	printf("keycode: %d\n", keycode);
-	if (keycode == 53) {
+	if (keycode == 53)
+	{
 		free_fdf(f);
 		exit(0);
 	}
-	if(keycode >= 18 && keycode <= 23)
+	else if (keycode >= 18 && keycode <= 23)
 		key_rotate(keycode, f);
-	if(keycode == 34 || keycode == 35)
+	else if (keycode == 34 || keycode == 35)
 		key_projection(keycode, f);
-	if(keycode == 43 || keycode == 47)
+	else if (keycode == 43 || keycode == 47)
 		key_altitude(keycode, f);
-	if(keycode >= 123 && keycode <= 126)
+	else if (keycode >= 123 && keycode <= 126)
 		key_offset(keycode, f);
-	// zoom in key 24
-	if(keycode == 24 || keycode == 69)
+	else if (keycode == 24 || keycode == 69)
 		zoom_in(f);
-	if (keycode == 27|| keycode == 78 )
+	else if (keycode == 27 || keycode == 78)
 		zoom_out(f);
-	// reset R = 15
-	if(keycode == 15)
+	else if (keycode == 15)
 		reset(f);
 	return (0);
 }
-
-
-
-
-
-
-
--- srcs/event/mouse.c
+++ srcs/event/mouse.c
@@ -6,16 +6,16 @@
 /*   By: ppimchan <ppimchan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 17:06:44 by ppimchan          #+#    #+#             */
-/*   Updated: 2023/07/16 15:04:03 by ppimchan         ###   ########.fr       */
+/*   Updated: 2023/07/16 20:33:43 by ppimchan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int mouse_move(int x, int y, t_fdf *f)
+int	mouse_move(int x, int y, t_fdf *f)
 {
-	int prev_x;
-	int prev_y;
+	int	prev_x;
+	int	prev_y;
 
 	prev_x = f->camera->x;
 	prev_y = f->camera->y;
@@ -31,15 +31,13 @@ int mouse_move(int x, int y, t_fdf *f)
 			f->camera->alpha += 0.02;
 		else if (y - prev_y < 0)
 			f->camera->alpha -= 0.02;
-
 		rerender(f);
 	}
 	return (0);
 }
 
-int mouse_press(int button, int x, int y, t_fdf *f)
+int	mouse_press(int button, int x, int y, t_fdf *f)
 {
-
 	f->camera->x = x;
 	f->camera->y = y;
 	if (button == 5)
@@ -53,17 +51,16 @@ int mouse_press(int button, int x, int y, t_fdf *f)
 			f->camera->zoom -= 1;
 		rerender(f);
 	}
-
 	if (button == 1)
 		f->camera->is_press = 1;
 	return (0);
 }
 
-int mouse_release(int button, int x, int y, t_fdf *f)
+int	mouse_release(int button, int x, int y, t_fdf *f)
 {
 	f->camera->x = x;
 	f->camera->y = y;
-	if(button == 1)
+	if (button == 1)
 		f->camera->is_press = 0;
 	return (0);
 }
